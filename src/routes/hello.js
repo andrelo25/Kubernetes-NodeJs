0f92030a866dcd69de8b4584ffa7e96673d49267
Andres Nicolas Herrera <Andres.Nicolas.Herrera@ibm.com>
2022-01-07 16:57:00 -0300

    se modifica el mensaje de bienvenida

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 router.route('/').get((req, res) => {
-    msg = `Hello ${req.query.name || "World"} from get request!`;
+    msg = `Hola ${req.query.name || "Mundo"} desde get request!`;
     res.json({msg});
   })
 router.route('/:lang').get((req, res) => {
